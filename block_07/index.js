f3df0bee1f633d332af0ee31efa04ab99da078f8
rblanc6 <rab.nola@gmail.com>
2024-09-26 19:41:40 -0500

    secure-the-vault

---
+++ block_07/index.js
@@ -0,0 +1,24 @@
+/*
+Pseudocode
+1. Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+2. Assign three variables for each number in the combination: 10 - 40 - 39
+3. Combine the message with the combination variables
+4. Create an alert for the user to display the message with combination
+*/
+
+const first = 8 + 2; // uses addition to display first number in combination
+const second = 10 * 4; // uses multiplication to display first number in combination
+const third = 40 - 1; // uses subtraction to display first number in combination
+
+const message =
+  "You have You have received this message because you have been chosen to open an important vault. Here is the secret combination:" +
+  first +
+  "-" +
+  second +
+  "-" +
+  third;
+  // combines the message with combination
+
+alert(message);
+// displays message to user
+
