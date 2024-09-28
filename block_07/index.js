823ec2fb6c1a697c358bb8158c268fa1fece7cef
rblanc6 <rab.nola@gmail.com>
2024-09-28 11:42:34 -0500

    final changes

--- block_07/index.js
+++ block_07/index.js
@@ -6,10 +6,14 @@ Pseudocode
 4. Create an alert for the user to display the message with combination
 */
 
-const first = 8 + 2; // uses addition to display first number in combination
-const second = 10 * 4; // uses multiplication to display first number in combination
-const third = 40 - 1; // uses subtraction to display first number in combination
+// uses addition to display first number in combination
+const first = 8 + 2;
+// uses multiplication to display first number in combination
+const second = 10 * 4;
+// uses subtraction to display first number in combination
+const third = 40 - 1;
 
+// combines the message with combination
 const message =
   "You have You have received this message because you have been chosen to open an important vault. Here is the secret combination:" +
   first +
@@ -17,8 +21,6 @@ const message =
   second +
   "-" +
   third;
-  // combines the message with combination
 
-alert(message);
 // displays message to user
-
+alert(message);
